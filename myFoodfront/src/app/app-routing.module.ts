d61a891b0f72064f73818383ab7eb899ae973823
samAkkari <sameh.akkari@esprit.com.tn>
2021-07-29 00:15:04 +0100

    sameh

--- myFoodfront/src/app/app-routing.module.ts
+++ myFoodfront/src/app/app-routing.module.ts
@@ -7,6 +7,13 @@ import {ReclamationComponent} from './reclamation/reclamation.component';
 import {HomeActiviteComponent} from './home-activite/home-activite.component';
 import {FormActiviteComponent} from './form-activite/form-activite.component';
 import {AccueilComponent} from './accueil/accueil.component';
+import {FormProduitComponent} from './form-produit/form-produit.component';
+import {ProduitComponent} from './produit/produit.component';
+import {ListProduitComponent} from './list-produit/list-produit.component';
+import {HomeRecetteComponent} from './home-recette/home-recette.component';
+import {FormRecetteComponent} from './form-recette/form-recette.component';
+
+
 
 
 const ROUTES: Routes = [
@@ -33,8 +40,22 @@ const ROUTES: Routes = [
   },
   {
     path: 'accueil', component: AccueilComponent
-  }
-
+  },
+  {
+    path: 'addProduit', component: FormProduitComponent
+  },
+  {
+    path: 'produit', component: ProduitComponent
+  },
+  {
+    path: 'homeProduit', component: ListProduitComponent
+  },
+  {
+    path: 'homeRecette', component: HomeRecetteComponent
+  },
+  {
+    path: 'addRecette', component: FormRecetteComponent
+  },
 ];
 
 @NgModule({
--- myFoodfront/src/app/app.module.ts
+++ myFoodfront/src/app/app.module.ts
@@ -16,6 +16,12 @@ import { HomeActiviteComponent } from './home-activite/home-activite.component';
 import { FormActiviteComponent } from './form-activite/form-activite.component';
 import {IvyCarouselModule} from 'angular-responsive-carousel';
 import { AccueilComponent } from './accueil/accueil.component';
+import { ProduitComponent } from './produit/produit.component';
+import { ListProduitComponent } from './list-produit/list-produit.component';
+import { FormProduitComponent } from './form-produit/form-produit.component';
+import { RecetteComponent } from './recette/recette.component';
+import { FormRecetteComponent } from './form-recette/form-recette.component';
+import { HomeRecetteComponent } from './home-recette/home-recette.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +34,13 @@ import { AccueilComponent } from './accueil/accueil.component';
     ActiviteComponent,
     HomeActiviteComponent,
     FormActiviteComponent,
-    AccueilComponent
+    AccueilComponent,
+    ProduitComponent,
+    ListProduitComponent,
+    FormProduitComponent,
+    RecetteComponent,
+    FormRecetteComponent,
+    HomeRecetteComponent
   ],
   imports: [
     BrowserModule,
